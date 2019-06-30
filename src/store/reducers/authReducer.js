2da29abeccb0eb332f744622eb6ae0720baa8ae5
Keseljs <keseljstrahinja@gmail.com>
2019-06-30 20:06:50 +0200

    Updating Routes

--- src/store/reducers/authReducer.js
+++ src/store/reducers/authReducer.js
@@ -1,7 +1,31 @@
-const initState = {}
+const initState = {
+    authError: null,
+}
 
 const authReducer = (state = initState, action) => {
-    return state;
+
+    switch(action.type){
+        case 'LOGIN_ERROR':
+            console.log('login error')
+            return {
+                ...state,
+                authError: 'Login Failed'
+            }
+        case 'LOGIN_SUCCESS':
+            console.log('login succ')    
+            return {
+                ...state,
+                authError: null
+            }
+            case 'SIGNOUT_SUCCESS':
+            console.log('signed out')   
+            return state;
+        default:
+            return state;    
+
+    }
+
+    
 }
 
 export default authReducer;
