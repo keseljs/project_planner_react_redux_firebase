b7a51bcc74181f066d08141a0c39e386917d9534
Strahinja <keseljstrahinja@gmail.com>
2019-07-03 19:56:36 +0200

    Final Touches

---
+++ dist/precache-manifest.81984a8ecff80d50b2fa94a18a7e74d7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3a025d80f56686b3e13fbe6aa7f7b33",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36373be505a8b2c179c3",
+    "url": "/static/css/main.5ef17ca6.chunk.css"
+  },
+  {
+    "revision": "6fb37649f0e0671ec58c",
+    "url": "/static/js/2.c9aa0231.chunk.js"
+  },
+  {
+    "revision": "36373be505a8b2c179c3",
+    "url": "/static/js/main.dc0673c7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -4,13 +4,13 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-	apiKey: 'AIzaSyAJJtS3i0RuXqbfmO-sxPaAwzxKpIbJUrA',
-	authDomain: 'net-ninja-marioplan-ebeb5.firebaseapp.com',
-	databaseURL: 'https://net-ninja-marioplan-ebeb5.firebaseio.com',
-	projectId: 'net-ninja-marioplan-ebeb5',
-	storageBucket: '',
-	messagingSenderId: '806720838851',
-	appId: '1:806720838851:web:bc4c0d62ba5aa61e'
+	apiKey: "AIzaSyCNKVAJORTAavJ4qH2Zr24hAzZTGb8hrfQ",
+    authDomain: "project-planner-636fd.firebaseapp.com",
+    databaseURL: "https://project-planner-636fd.firebaseio.com",
+    projectId: "project-planner-636fd",
+    storageBucket: "",
+    messagingSenderId: "376454412577",
+    appId: "1:376454412577:web:46171a09377c838e"
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
